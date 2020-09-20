40808451fab60dc6069985e837cce9bff9c776dd
Andres Ruiz <andrsruiz@gmail.com>
2020-09-20 10:54:12 -0500

    Show background in Scene

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import Phaser from 'phaser'
 
 let gameScene = new Phaser.Scene('Game')
 
+gameScene.preload = function(){
+	this.load.image('background', '../src/assets/bg_castle.png')
+}
+
+gameScene.create = function(){
+	this.add.sprite(640/2, 360/2, 'background')
+}
+
 let config = {
 	type: Phaser.AUTO,
 	width: 640,
